b7e884d16b8f0d76012ac6f3cfdfb840b1b7c21d
Devraj <dvrjchavan@gmail.com>
2023-06-06 14:40:05 +0530

    make it better

--- helper.py
+++ helper.py
@@ -1,11 +1,16 @@
+from collections import Counter
+
+import emoji
+import networkx as nx
+import pandas as pd
+from matplotlib import pyplot as plt
+from networkx.algorithms import community
+from textblob import TextBlob
 from urlextract import URLExtract
 from wordcloud import WordCloud
-import pandas as pd
-from collections import Counter
-import emoji
+import community as community_louvain
 
 extract = URLExtract()
-
 def fetch_stats(selected_user,df):
 
     if selected_user != 'Overall':
@@ -140,7 +145,57 @@ def activity_heatmap(selected_user,df):
 
 
 
+#new
+def perform_sentiment_analysis(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
 
+    # Perform sentiment analysis on each message
+    sentiment_scores = []
+    for message in df['message']:
+        blob = TextBlob(message)
+        sentiment_scores.append(blob.sentiment.polarity)
 
+    # Add sentiment scores to the DataFrame
+    df['sentiment_score'] = sentiment_scores
 
+    return df
 
+#new
+def identify_influential_users(df):
+    # Create a directed graph
+    G = nx.DiGraph()
+
+    # Add edges between users based on chat data
+    G.add_edges_from(zip(df['user'].shift(), df['user']))
+
+    # Calculate eigenvector centrality to identify influential users
+    centrality = nx.eigenvector_centrality(G)
+
+    # Sort users based on centrality values
+    sorted_users = sorted(centrality, key=centrality.get, reverse=True)
+
+    return sorted_users
+
+def detect_communities(df):
+    # Create an undirected graph
+    G = nx.Graph()
+
+    # Add edges between users based on chat data
+    G.add_edges_from(zip(df['user'].shift(), df['user']))
+
+    # Detect communities using Louvain algorithm
+    communities = community_louvain.best_partition(G)
+
+    return communities
+
+def visualize_interaction_network(df):
+    # Create a graph
+    G = nx.Graph()
+
+    # Add edges between users based on chat data
+    G.add_edges_from(zip(df['user'].shift(), df['user']))
+
+    # Plot the interaction network
+    nx.draw(G, with_labels=True)
+    plt.show()
